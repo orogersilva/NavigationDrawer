17731193305a070e14e6ee137ab207f84adb8df5
Johan <johank@dbserver.com.br>
2018-08-02 11:09:41 -0300

    Added NewGameActivity

---
+++ app/src/main/java/kovalsikoski/johan/navigationdrawer/NewGameActivity.kt
@@ -0,0 +1,11 @@
+package kovalsikoski.johan.navigationdrawer
+
+import android.os.Bundle
+
+class NewGameActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_game)
+    }
+}
